70e1928bbf2d7c8616ec781bdf6b536a1c96dd8e
Stanislav Feldman <stanislavfeldman@gmail.com>
2011-11-24 16:56:30 +0400

    add header to gevent app

--- python/gev.py
+++ python/gev.py
@@ -3,8 +3,9 @@ from gevent import wsgi
 class Application(object):
 	def on_request(self, env, res):
 		print env
-		res("200 OK", [])
-		return ["hello))"]
+		html = "<h1>hello))</h1>"
+		res("200 OK", [("Content-Type", "text/html")])
+		return [html]
 		
 if __name__ == "__main__":
 	app = Application()
