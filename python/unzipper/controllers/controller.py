fa62e7224263b7712cbca0bb00050d103b18b69e
Stanislav Feldman <stanislavfeldman@gmail.com>
2011-11-28 23:27:13 +0400

    you can view zip contents

--- python/unzipper/controllers/controller.py
+++ python/unzipper/controllers/controller.py
@@ -1,5 +1,5 @@
 from kiss.views.templates import TemplateResponse
-from kiss.controllers.router import Response
+from kiss.views.base import Response
 import os
 from zipfile import *
 		
@@ -8,14 +8,14 @@ class Controller(object):
 		self.tmp_file_path = './tmp_file.zip'
 		
 	def get(self, request):
-		print request.session
 		return TemplateResponse("view.html")
 	
 	def post(self, request):
 		file = request.files.get('zipfile')
 		file.save(self.tmp_file_path)
+		namelist = []
 		with ZipFile(self.tmp_file_path, "r") as myzip:
 			for item in myzip.namelist():
-				print item
+				namelist.append(item)
 		os.remove(self.tmp_file_path)
-		return TemplateResponse("view.html")
+		return TemplateResponse("view.html", {"files": namelist})
