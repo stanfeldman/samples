6ed9f184c0222d9fa203af7f36cfd723c9a532eb
Stanislav Feldman <stanislavfeldman@gmail.com>
2011-12-15 16:36:34 +0400

    modify calc example and add visitor pattern sample

--- python/calc.py
+++ python/calc.py
@@ -1,137 +1,150 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
-if sys.version_info[0] >= 3:
-    raw_input = input
-import ply.lex as lex
-import ply.yacc as yacc
-import os
+from ply.lex import lex
+from ply.yacc import yacc
+from abc import ABCMeta, abstractproperty
 import re
-
+import sys
 
 class Parser(object):
-    """
-    Base class for a lexer/parser that has the rules defined as methods
-    """
-    tokens = ()
-    precedence = ()
+	"""
+	Абстрактный парсер, нужно наследоваться и определить tokens, t_* и p_*
+	"""
+	__metaclass__ = ABCMeta
+	
+	def __init__(self):
+		self.modname = "_" + self.__class__.__name__ + "_parser"
+		self.tabmodule = self.modname + "_table"
+		self.lex = lex(module=self, reflags=re.UNICODE)
+		self.yacc = yacc(module=self, tabmodule=self.tabmodule)
+	
+	@abstractproperty
+	def tokens(self):
+		pass
+		
+	t_ignore = " \t"
+	
+	def t_error(self, t):
+		print "Неправильный символ %s" % t.value
+		t.lexer.skip(1)
+		
+	def parse(self, input):
+		self.yacc.parse(input)
+		
+class Calculator(Parser):
+	def __init__(self):
+		super(Calculator, self).__init__()
+		self.names = {}
+		
+	"""
+	Лексический анализатор
+	Нужно определить названия токенов tokens,
+	согласно этим названия определить регулярные выражения, соответсвующие токенам t_*
+	"""
+	
+	tokens = (
+		"name",
+		"eq",
+		"number",
+		"plus",
+		"minus",
+		"times",
+		"div",
+		"lparen",
+		"rparen"
+	)
+	
+	t_name = ur"\w+"
+	t_eq = ur"="
+	t_plus = ur"\+"
+	t_minus = ur"-"
+	t_times = ur"\*"
+	t_div = ur"/"
+	t_lparen = ur"\("
+	t_rparen = ur"\)"
+		
+	def t_number(self, t):
+		ur"\d+"
+		t.value = int(t.value)
+		return t
+		
+	"""
+	Синтаксический анализатор
+	БНФ:
+	statement	:	name eq expression 
+				|	expression
+	expression	:	expression plus term
+				|	expression minus term
+				|	term
+	term		:	term times factor
+				|	term div factor
+				|	factor
+	factor		:	name
+				|	number
+				|	lparen expression rparen
+	"""
+	
+	def p_statement_eq(self, p):
+		"statement : name eq expression"
+		self.names[p[1]] = p[3]
+		
+	def p_statement_expression(self, p):
+		"statement : expression"
+		print p[1]
+		
+	def p_expression_plus(self, p):
+		"expression : expression plus term"
+		p[0] = p[1] + p[3]
+		
+	def p_expression_minus(self, p):
+		"expression : expression minus term"
+		p[0] = p[1] - p[3]
+		
+	def p_expression_term(self, p):
+		"expression : term"
+		p[0] = p[1]
+		
+	def p_term_times(self, p):
+		"term : term times factor"
+		p[0] = p[1] * p[3]
+		
+	def p_term_div(self, p):
+		"term : term div factor"
+		p[0] = p[1] / p[3]
+	
+	def p_term_factor(self, p):
+		"term : factor"
+		p[0] = p[1]
+		
+	def p_factor_name(self, p):
+		"factor : name"
+		try:
+			p[0] = self.names[p[1]]
+		except:
+			print "Неизвестная переменная %s" % p[1]
+			p[0] = 0
+	
+	def p_factor_number(self, p):
+		"factor : number"
+		p[0] = p[1]
+		
+	def p_factor_expression(self, p):
+		"factor : lparen expression rparen"
+		p[0] = p[2]
+		
+	def p_error(self, p):
+		print "Неверный синаксис %s" % p
+	
+	def run(self):
+		while True:
+			try:
+				input = raw_input("калькулятор> ").decode(sys.stdin.encoding)
+			except KeyboardInterrupt:
+				print "\nдо свидания!"
+				break
+			if input:
+				self.parse(unicode(input))
 
-    def __init__(self, **kw):
-        self.debug = kw.get('debug', 0)
-        self.names = {}
-        modname = "_" + self.__class__.__name__ + "_parser"
-        self.debugfile = modname + "_dbg"
-        self.tabmodule = modname + "_parsetab"
-        # Build the lexer and parser
-        lex.lex(module=self, debug=self.debug, reflags=re.UNICODE)
-        yacc.yacc(module=self,
-                  debug=self.debug,
-                  debugfile=self.debugfile,
-                  tabmodule=self.tabmodule)
-
-    def run(self):
-        while 1:
-            try:
-                s = raw_input('calc > ').decode(sys.stdin.encoding)
-            except EOFError:
-                break
-            if not s: continue     
-            yacc.parse(unicode(s))
-
-    
-class Calc(Parser):
-    tokens = (
-        'NAME','NUMBER',
-        'PLUS','MINUS','EXP', 'TIMES','DIVIDE','EQUALS',
-        'LPAREN','RPAREN',
-        )
-    # Tokens
-    t_PLUS    = ur'\+'
-    t_MINUS   = ur'-'
-    t_EXP     = ur'\*\*'
-    t_TIMES   = ur'\*'
-    t_DIVIDE  = ur'/'
-    t_EQUALS  = ur'='
-    t_LPAREN  = ur'\('
-    t_RPAREN  = ur'\)'
-    t_NAME    = ur'\w+'
-
-    def t_NUMBER(self, t):
-        ur'[0-9]+\.?[0-9]*'
-        try:
-        	t.value = float(t.value)
-        except ValueError:
-            print("Float value too large %s" % t.value)
-            t.value = 0
-        return t
-
-    t_ignore = u" \t"
-
-    def t_newline(self, t):
-        ur'\n+'
-        t.lexer.lineno += t.value.count("\n")
-    
-    def t_error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
-
-    # Parsing rules
-    precedence = (
-        ('left','PLUS','MINUS'),
-        ('left','TIMES','DIVIDE'),
-        ('left', 'EXP'),
-        ('right','UMINUS'),
-        )
-
-    def p_statement_assign(self, p):
-        'statement : NAME EQUALS expression'
-        self.names[p[1]] = p[3]
-
-    def p_statement_expr(self, p):
-        'statement : expression'
-        print(p[1])
-
-    def p_expression_binop(self, p):
-        """
-        expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression
-                  | expression EXP expression
-        """
-        if p[2] == u'+'  : p[0] = p[1] + p[3]
-        elif p[2] == u'-': p[0] = p[1] - p[3]
-        elif p[2] == u'*': p[0] = p[1] * p[3]
-        elif p[2] == u'/': p[0] = p[1] / p[3]
-        elif p[2] == u'**': p[0] = p[1] ** p[3]
-
-    def p_expression_uminus(self, p):
-        'expression : MINUS expression %prec UMINUS'
-        p[0] = -p[2]
-
-    def p_expression_group(self, p):
-        'expression : LPAREN expression RPAREN'
-        p[0] = p[2]
-
-    def p_expression_number(self, p):
-        'expression : NUMBER'
-        p[0] = p[1]
-
-    def p_expression_name(self, p):
-        'expression : NAME'
-        try:
-            p[0] = self.names[p[1]]
-        except LookupError:
-            print("Undefined name '%s'" % p[1])
-            p[0] = 0
-
-    def p_error(self, p):
-        if p:
-            print("Syntax error at '%s'" % p.value)
-        else:
-            print("Syntax error at EOF")
-
-
-if __name__ == '__main__':
-    calc = Calc()
-    calc.run()
+if __name__ == "__main__":
+	calc = Calculator()
+	calc.run()
+	
