f8bf4fd5ec9f42ea88d10c4aab262767ac9716f1
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-04-20 12:34:50 +0800

    add example for finite state machine

--- python/fsm.py
+++ python/fsm.py
@@ -41,8 +41,40 @@ def nfsmsim(string, current, edges, accepting):
 						return result
 		return False
 
-print "Test case 1 passed: " + str(nfsmsim("abc", 1, edges, accepting) == True) 
-print "Test case 2 passed: " + str(nfsmsim("aaa", 1, edges, accepting) == True) 
-print "Test case 3 passed: " + str(nfsmsim("abbbc", 1, edges, accepting) == True) 
-print "Test case 4 passed: " + str(nfsmsim("aabc", 1, edges, accepting) == False) 
-print "Test case 5 passed: " + str(nfsmsim("", 1, edges, accepting) == False) 
+#print "Test case 1 passed: " + str(nfsmsim("abc", 1, edges, accepting) == True) 
+#print "Test case 2 passed: " + str(nfsmsim("aaa", 1, edges, accepting) == True) 
+#print "Test case 3 passed: " + str(nfsmsim("abbbc", 1, edges, accepting) == True) 
+#print "Test case 4 passed: " + str(nfsmsim("aabc", 1, edges, accepting) == False) 
+#print "Test case 5 passed: " + str(nfsmsim("", 1, edges, accepting) == False)
+
+edges = { (1, 'a') : [2, 3],
+          (2, 'a') : [2],
+          (3, 'b') : [4, 2],
+          (4, 'c') : [5] }
+accepting = [5] 
+edges2 = { (1, 'a') : [1],
+           (2, 'a') : [2] }
+accepting2 = [2]
+
+
+def nfsmaccepts(current, edges, accepting, visited):
+	if current in accepting:
+		return ""
+	else:
+		visited.append(current)
+		path = ""
+		for (node,label), next in edges.items():
+			for dest in next:
+				if current == node and dest not in visited:
+					new_path = nfsmaccepts(dest, edges, accepting, visited)
+					if new_path is not None:
+						path += label + new_path
+		return None if not path else path
+			
+acs = nfsmaccepts(1, edges, accepting, [])
+print acs
+print nfsmsim(acs, 1, edges, accepting)
+print "Test 1: " + str(nfsmaccepts(1, edges, accepting, []) == "abc") 
+print "Test 2: " + str(nfsmaccepts(1, edges, [4], []) == "ab") 
+print "Test 3: " + str(nfsmaccepts(1, edges2, accepting2, []) == None) 
+print "Test 4: " + str(nfsmaccepts(1, edges2, [1], []) == "")
---
+++ python/pub_sub.py
@@ -0,0 +1,40 @@
+from gevent import monkey
+monkey.patch_all()
+from gevent import Greenlet, joinall
+from kombu import BrokerConnection, Exchange, Queue
+from kombu.exceptions import TimeoutError
+from time import time
+
+def publish():
+	print "start publishing"
+	start_time = time()
+	with BrokerConnection("amqp://guest:guest@localhost//") as connection:
+		exchange = Exchange("addstore", "direct", durable=True)
+		with connection.Producer(exchange=exchange, serializer="json", routing_key="grabber", auto_declare=True) as producer:
+			body = {"name": "stas", "age": 23}
+			producer.publish(body)
+			print "published: %s" % body
+			print "publish time: %s ms" % ((time()-start_time)*1000)
+def consume():
+	print "start consuming"
+	start_time = time()
+	def callback(body, message):
+		print "consumed: %s" % body
+		message.ack()
+		print "consume time: %s ms" % ((time()-start_time)*1000)
+	with BrokerConnection("amqp://guest:guest@localhost//") as connection:
+		exchange = Exchange("addstore", "direct", durable=True)
+		queue = Queue("grabber", exchange=exchange)
+		with connection.Consumer([queue], callbacks=[callback], auto_declare=True) as consumer:
+			while(True):
+				try:
+					connection.drain_events(timeout=1)
+				except TimeoutError:
+					break
+					
+consumer = Greenlet(consume)
+publisher = Greenlet(publish)
+consumer.start()
+publisher.start()
+print "app started"
+joinall([publisher, consumer])
