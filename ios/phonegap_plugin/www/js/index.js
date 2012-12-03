ff0499f2c92a5bf6fb1e0495c35e5127df727819
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-12-03 17:01:01 +0400

    tried to get ios contacts from phonegap

--- ios/phonegap_plugin/www/js/index.js
+++ ios/phonegap_plugin/www/js/index.js
@@ -14,6 +14,7 @@ var app = {
         }, function(err){
             console.log("error Plugin1.first: " + err);
         });
+        app.get_contacts();
     },
     report: function(id) { 
         console.log("report:" + id);
@@ -21,5 +22,33 @@ var app = {
         document.querySelector('#' + id + ' .pending').className += ' hide';
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
+    },
+    get_contacts: function(){
+        var need_contact = {phone: "+79169534399", email: "test@gmail.com"};
+        var callback = function(contacts){
+            for(var i = 0; i < contacts.length; ++i){
+                var contact = contacts[i];
+                if(contact.emails)
+                    for(var j = 0; j < contact.emails.length; ++j){
+                        var e = contact.emails[j].value;
+                        if(e == need_contact.email){
+                            console.log("hey! " + contact.name.formatted);
+                            contact.nickname = "founded";
+                            contact.save();
+                        }
+                    }
+                // console.log("name: " + JSON.stringify(contact.name) + 
+                //             "; phoneNumbers: " + JSON.stringify(contact.phoneNumbers) + 
+                //             "; emails: " + JSON.stringify(contact.emails));
+            }
+        };
+        var errback = function(err){
+            console.log("contacts error: " + err);
+        };
+        var fields = ["name", "emails", "phoneNumbers"];
+        var options = new ContactFindOptions();
+        options.filter="";
+        options.multiple=true;
+        navigator.contacts.find(fields, callback, errback, options);
     }
 };
