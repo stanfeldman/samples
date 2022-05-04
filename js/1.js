3dab8e7e7ebd162ea1c5d18269c0f10cd8904db8
Stan Feldman <stan.feldman@sendle.com>
2022-05-04 20:28:28 +0800

    a change

--- js/1.js
+++ js/1.js
@@ -1,56 +1,50 @@
-console.log("hi");
+console.log("hello");
 var x = 7;
-function sum(a, b) 
-{
+function sum(a, b) {
 	console.log(arguments);
-	console.log(arguments.callee); 
-	return a+b; 
+	console.log(arguments.callee);
+	return a + b;
 }
 console.log(sum(3, 5));
-function fac(num)
-{
-	if(num <= 1)
+function fac(num) {
+	if (num <= 1)
 		return 1;
 	else
-		return num * fac(num-1);
+		return num * fac(num - 1);
 }
 console.log(fac(99));
 var calc = {
 	op1: 555,
 	op2: 5,
-	sum: function() { return this.op1 + this.op2; }
+	sum: function () { return this.op1 + this.op2; }
 }
 console.log(calc.sum());
-console.log(calc.sum.call({op1: 4, op2:7}));
-function get_func(x)
-{
+console.log(calc.sum.call({ op1: 4, op2: 7 }));
+function get_func(x) {
 	var y = 0;
 	y += x;
-	return function() { return y; }
+	return function () { return y; }
 }
 console.log(get_func(1)());
 console.log(get_func(2)());
-uid = (function()
-{
+uid = (function () {
 	var id = 0;
-	return function() { return id++; }
+	return function () { return id++; }
 })();
 console.log(uid());
 console.log(uid());
 
-function Rect(w, h)
-{
+function Rect(w, h) {
 	this.w = w;
 	this.h = h;
 }
-Rect.prototype.area = function() { return this.w*this.h; }
+Rect.prototype.area = function () { return this.w * this.h; }
 
-r = new Rect(5,6);
+r = new Rect(5, 6);
 console.log(r.area());
 console.log(r.hasOwnProperty("area"));
 
-function PosRect(x, y, w, h)
-{
+function PosRect(x, y, w, h) {
 	Rect.call(this, w, h);
 	this.x = x;
 	this.y = y;
@@ -59,12 +53,11 @@ PosRect.prototype = new Rect();
 delete PosRect.prototype.w;
 delete PosRect.prototype.h;
 PosRect.prototype.constructor = PosRect;
-PosRect.prototype.contains = function(x, y)
-{
+PosRect.prototype.contains = function (x, y) {
 	return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h);
 }
 
 pr = new PosRect(2, 2, 4, 4);
-console.log(pr.contains(3,3));
+console.log(pr.contains(3, 3));
 console.log(pr.area());
 console.log(Object.prototype.toString.apply(pr));
